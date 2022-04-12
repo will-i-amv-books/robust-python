1f12808878b179f726648a0827c55d9234f904fa
William <wsebasvb@gmail.com>
2022-04-12 09:58:16 -0400

    Add notebook with snippets from chapter 5, along with auxiliary scripts

---
+++ chapter05/graph.py
@@ -0,0 +1,39 @@
+from typing import Union, TypeVar, Generic
+from collections import defaultdict
+
+
+Ingredient = tuple[str, int, str]
+Recipe = tuple[Union[int, Ingredient]]
+Cookbook = tuple[str, int]
+
+Node = TypeVar("Node")
+Edge = TypeVar("Edge")
+
+
+class Graph(Generic[Node, Edge]):
+    def __init__(self):
+        self.edges: dict[Node, list[Edge]] = defaultdict(list)
+
+    def add_relation(self, node: Node, to: Edge):
+        self.edges[node].append(to)
+
+    def get_relations(self, node: Node) -> list[Edge]:
+        return self.edges[node]
+
+
+recipes: Graph[Recipe, Recipe] = Graph()
+cookbook_recipes: Graph[Cookbook, Recipe] = Graph()
+cookbooks: Graph[Cookbook, Cookbook] = Graph()
+
+recipes.add_relation(
+    (('Pasta Bolognese', 1, 'dish'), ), 
+    (('Pasta with Sausage and Basil', 2, 'dish'), )
+)
+cookbook_recipes.add_relation(
+    ('The Food Lab', 1), 
+    (('Pasta Bolognese', 3, 'dish'), )
+)
+cookbooks.add_relation(
+    (('Cheeseburger', 3, 'dish'), ), 
+    (('Hamburger', 4, 'dish'), )
+)
